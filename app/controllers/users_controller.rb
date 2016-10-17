f4bac32bb6a4baab8c69ca37e5570dd4229ee9af
prad2438 <pradeep.ravela@rackspace.com>
2016-10-17 00:53:14 -0500

    Edit, Update & Delete Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
     # render text: params[:user]
-    @user = User.create( params.require(:user).permit(:name) )
+    @user = User.create(params.require(:user).permit(:name))
     redirect_to users_path
   end
 
@@ -13,10 +13,27 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def edit
-    render text: params['id']
+    # render text: params['id']
     @user = User.find_by_id(params['id'])
-    @user.save
-    # render text: @edit
+  end
+
+  def update
+    # render text: params
+    @user = User.find(params['id'])
+    @user.update_attributes(params.require(:user).permit(:name))
+    #@user.update_attributes(params['id'])
+    redirect_to users_path
+  end
+
+  def destroy
+    # render text: params
+    @user = User.find(params['id'])
+    @user.destroy
+    redirect_to users_path
   end
 end
